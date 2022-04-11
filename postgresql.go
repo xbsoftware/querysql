a26e035e615715e77c03c81f73caa65ab29f77a4
Andrey Shpak <ashpak@xbsoftware.by>
2022-04-11 18:40:18 +0300

    [dev] refacoring

--- postgresql.go
+++ postgresql.go
@@ -21,32 +21,26 @@ func (m *PostgreSQL) Mark() string {
 
 func (m *PostgreSQL) IsJSON(v string) (string, bool) {
 	//table.json:field.name:type
-	var table, field, name, tp string
+	var table, field, name string
+	var meta []string
+	tp := "text"
 
 	parts := strings.SplitN(v, ".", 3)
 	if len(parts) == 2 && strings.HasPrefix(parts[0], "json:") {
 		// [0]:json:field, [1]:name:type
 		field = parts[0][5:]
-		i := strings.Index(parts[1], ":")
-		if i > 0 {
-			name = parts[1][:i]
-			tp = parts[1][i+1:]
-		} else {
-			name = parts[1]
-		}
+		meta = strings.SplitN(parts[1], ":", 2)
 	} else if len(parts) == 3 && strings.HasPrefix(parts[1], "json:") {
 		// [0]:table, [1]:json:field, [2]:name:type
 		table = parts[0]
 		field = parts[1][5:]
-		i := strings.Index(parts[2], ":")
-		if i > 0 {
-			name = parts[2][:i]
-			tp = parts[2][i+1:]
-		} else {
-			name = parts[2]
-		}
+		meta = strings.SplitN(parts[2], ":", 2)
 	} else {
 		return v, false
+	}
+	name = meta[0]
+	if len(meta) == 2 {
+		tp = meta[1]
 	}
 
 	var s, e string
